c0718c888bd8165500b8afc5a5abd55604b2a7fb
Jucan <jucan_rares@yahoo.com>
2023-11-07 00:57:40 +0200

    this is the functional, stable version

--- uid.js
+++ uid.js
@@ -1,126 +1,164 @@
 document.getElementById('displayButton').addEventListener('click', function () {
 
-    hide('displayButton');
-    hide('content1');
-    hide('content2');
-    hide('resultsLimit');
-    hide('limitButton');
-    show('loading');
+    loading();
 
-    // Here we set everything back to normal after the loading is complete
     setTimeout(function () {
-        show('displayButton');
         show('content1');
-        show('resultsLimit');
-        show('limitButton');
-        hide('loading');
+        afterLoading();
     }, 3000);
 
+    fetchResultsBasic();
+});
+document.getElementById('limitButton').addEventListener('click', function () {
+
+    const resultsLimit = document.getElementById('resultsLimit').value;
+
+    if (resultsLimit >= 1 && resultsLimit <= 200) {
+        loading();
+        hide('error');
+
+        setTimeout(function () {
+            show('content2');
+            afterLoading();
+        }, 3000);
+
+        fetchResultsWithLimit(resultsLimit);
+    } else {
+        document.getElementById('error').innerHTML = 'Limita trebuie să fie între 1 și 200';
+        hide('content2');
+        show('error');
+    }
+});
+document.getElementById('searchButton').addEventListener('click', function () {
+
+    const searchTerm = document.getElementById('searchTerm').value.trim();
+
+    loading();
+
+    hide('error2');
+
+    setTimeout(function () {
+        show('content3');
+        afterLoading();
+    }, 3000);
+
+    fetchResultsByKeyword(searchTerm);
+});
+
+function fetchResultsBasic(){
     fetch('https://edtechbooks.org/api.php?action=search_books', {
         method: 'GET'
     })
     .then(res => {
         res.json()
             .then(data => {
-                if (data.status === 'success' && data.books) {
-                    var temp = "";
-                    for (const bookId in data.books) {
-                        const bookData = data.books[bookId];
-                        temp += "<tr>";
-                        temp += "<td>" + bookData.book_id + "</td>";  
-                        temp += "<td>" + bookData.title + "</td>";
-                        temp += "<td>" + bookData.subtitle + "</td>";     
-                        temp += "<td>" + bookData.links.html + "</td></tr>";
-                    }
-                    document.getElementById('data1').innerHTML = temp;  
+                renderBookListWithLinks(data, 'content1');
+            });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+function fetchResultsWithLimit(resultsLimit) {
+    fetch(`https://edtechbooks.org/api.php?action=search_books&offset=0&limit=${resultsLimit}`, {
+        method: 'GET'
+    })
+    .then(res => {
+        res.json()
+            .then(data => {
+                renderBookListWithLinks(data, 'content2');
+            });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+function fetchResultsByKeyword(searchTerm) {
+
+    fetch(`https://edtechbooks.org/api.php?action=search_books&term=${searchTerm}`, {
+        method: 'GET'
+    })
+    .then(res => {
+        res.json()
+            .then(data => {
+                renderBookListWithoutLinks(data, 'content3');
+                if (!data.books || Object.keys(data.books).length === 0) {
+                    document.getElementById('error2').innerHTML = 'Nu s-au găsit cărți pentru cuvântul cheie specificat.';
+                    hide('content3');
+                    show('error2');
                 }
             });
     })
     .catch(error => {
         console.error('Error:', error);
     });
-});
+}
 
-document.getElementById('limitButton').addEventListener('click', function () {
+function renderBookListWithLinks(data, element) {
+    if (data.status === 'success' && data.books) {
+        const bookList = document.getElementById(element);
+        bookList.innerHTML = '';
 
-    const resultsLimit = document.getElementById('resultsLimit').value;
-    
-    if (resultsLimit >= 1 && resultsLimit <= 200) {
+        Object.keys(data.books).forEach(bookId => {
+            const bookData = data.books[bookId];
+            const bookListItem = document.createElement('li');
+            const bookDetailsList = document.createElement('ul');
 
-        hide('displayButton');
-        hide('content1');
-        hide('content2');
-        hide('resultsLimit');
-        hide('limitButton');
-        hide('error');
-        show('loading');
+            const detailsToDisplay = ["book_id", "title", "subtitle", "links"];
+            detailsToDisplay.forEach(key => {
+                if (key === "links" && bookData[key]) {
+                    const linksList = document.createElement('ul');
 
-        //here we set everything back to normal after the loading is complete
-        setTimeout(function () {
-            show('displayButton');
-            show('content2');
-            show('resultsLimit');
-            show('limitButton');
-            hide('loading');
-        }, 3000);
-        
-        fetch(`https://edtechbooks.org/api.php?action=search_books&offset=0&limit=${resultsLimit}`, {
-            method: 'GET'
-        })
-        .then(res => {
-            res.json()
-                .then(data => {
-                    if (data.status === 'success' && data.books) {
-                        var temp = "";
-                        for (const bookId in data.books) {
-                            const bookData = data.books[bookId];
-                            temp += "<tr>";
-                            temp += "<td>" + bookData.book_id + "</td>";
-                            temp += "<td>" + bookData.title + "</td>";
-                            temp += "<td>" + bookData.subtitle + "</td>";
-                            temp += "<td>" + bookData.links.html + "</td></tr>";
+                    for (const linkKey in bookData.links) {
+                        const linkValue = bookData.links[linkKey];
+                        if (linkValue) {
+                            const linkListItem = document.createElement('li');
+                            linkListItem.innerHTML = `<strong>${linkKey}:</strong> ${linkValue}`;
+                            linksList.appendChild(linkListItem);
                         }
-                        document.getElementById('data2').innerHTML = temp;
                     }
-                });
-        })
-        .catch(error => {
-            console.error('Error:', error);
+
+                    if (linksList.childNodes.length > 0) {
+                        const linksListItem = document.createElement('li');
+                        linksListItem.innerHTML = '<strong>Links:</strong>';
+                        linksListItem.appendChild(linksList);
+                        bookDetailsList.appendChild(linksListItem);
+                    }
+                } else {
+                    const detailListItem = document.createElement('li');
+                    detailListItem.innerHTML = `<strong>${key}:</strong> ${bookData[key]}`;
+                    bookDetailsList.appendChild(detailListItem);
+                }
+            });
+
+            bookListItem.appendChild(bookDetailsList);
+            bookList.appendChild(bookListItem);
         });
-    } 
-    else {
-        document.getElementById('error').innerHTML = 'Limita trebuie să fie între 1 și 200';
-        show('error')
     }
-});
+}
+function renderBookListWithoutLinks(data, element) {
+    if (data.status === 'success' && data.books) {
+        const bookList = document.getElementById(element);
+        bookList.innerHTML = '';
 
-document.getElementById('searchButton').addEventListener('click', function () {
-    const searchTerm = document.getElementById('searchInput').value;
-    
-    // Fetch data based on the search term
-    fetch(`https://edtechbooks.org/api.php?action=search_books&term=${searchTerm}`, {
-        method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 'success' && data.books) {
-            // Display the pop-up with book details
-            const firstBook = data.books[Object.keys(data.books)[0]]; // Assuming you want details of the first book
-            showPopup(firstBook);
-        } else {
-            // Display an error message in the pop-up
-            showErrorMessage();
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+        Object.keys(data.books).forEach(bookId => {
+            const bookData = data.books[bookId];
+            const bookListItem = document.createElement('li');
+            const bookDetailsList = document.createElement('ul');
 
-document.getElementById('closePopup').addEventListener('click', function () {
-    // Close the pop-up when the "Închide" button is clicked
-    document.getElementById('popup').classList.add('hidden');
-});
+            const detailsToDisplay = ["book_id", "short_name", "title", "abstract"];
+            detailsToDisplay.forEach(key => {
+                
+                const detailListItem = document.createElement('li');
+                detailListItem.innerHTML = `<strong>${key}:</strong> ${bookData[key]}`;
+                bookDetailsList.appendChild(detailListItem);
+            });
+
+            bookListItem.appendChild(bookDetailsList);
+            bookList.appendChild(bookListItem);
+        });
+    }
+}
 
 function hide(elementId) {
     const element = document.getElementById(elementId);
@@ -134,15 +172,22 @@ function show(elementId) {
         element.style.display = 'block';
     }
 }
-function showPopup(book) {
-    document.getElementById('popupBookId').textContent = book.book_id;
-    document.getElementById('popupShortName').textContent = book.short_name;
-    document.getElementById('popupTitle').textContent = book.title;
-    document.getElementById('popupAbstract').textContent = book.abstract;
-    hide('popupErrorMessage');
-    show('popup');
-}
-function showErrorMessage() {
-    show('popupErrorMessage');
+function loading(){
+    hide('displayButton');
+    hide('content1');
+    hide('content2');
+    hide('resultsLimit');
+    hide('limitButton');
+    hide('searchTerm');
+    hide('searchButton');
     hide('popup');
+    show('loading');
+}
+function afterLoading(){
+    show('displayButton');
+    show('resultsLimit');
+    show('limitButton');
+    show('searchTerm');
+    show('searchButton');
+    hide('loading');
 }
